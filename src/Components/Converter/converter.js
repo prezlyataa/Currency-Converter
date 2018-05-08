7f7f2189af4030fd98b84a17aed2ed3e065a4717
Yuriy Prezlyata <yprezlyata@GW-GridDynamics.retn.net>
2018-05-08 15:36:43 +0200

    covert uah to usd and eur

--- src/Components/Converter/converter.js
+++ src/Components/Converter/converter.js
@@ -4,12 +4,35 @@ import { appService } from '../../services/getData';
 
 
 class Converter extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            data: []
-        }
+            data: [],
+            inputValue: '',
+            output_select: 'USD',
+            result: 0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeOutputSelect = this.handleChangeOutputSelect.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+    }
+
+    handleChangeOutputSelect(event) {
+        this.setState({
+            output_select: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.calculate();
     }
 
     getData(){
@@ -27,19 +50,35 @@ class Converter extends Component {
     componentWillMount(){
         this.getData();
     }
+    
+    calculate() {
+        const { inputValue, output_select, data }  = this.state;
+        switch (output_select) {
+            case 'USD':
+                this.setState({
+                    result: inputValue * data[0].sale
+                });
+                break;
+            case 'EURO':
+                this.setState({
+                    result: inputValue * data[1].sale
+                });
+                break;
+        }
+    }
 
 
     render(){
-        console.log(this.state.data)
+        console.log( this.state );
         return(
         <div className="main">
             <div className="main_header">
                 <h1>Currency Converter</h1>
             </div>  
-            <form action="javascript:void(0);">
+            <form action="javascript:void(0);" onSubmit={ this.handleSubmit }>
                 <div className="all">
                 <div className="in">
-                    <input type="number" min="0" step="0.01" id="input_curr" placeholder=" input"/>
+                    <input type="number" min="0" step="0.01" id="input_curr" placeholder=" input" value={ this.state.inputValue } onChange={ this.handleChange }/>
                     <select id="pick_input">
                     <option value="USD">USD</option>
                     <option value="EURO">EURO</option>
@@ -47,8 +86,8 @@ class Converter extends Component {
                     </select>
                 </div>
                 <div className="out">
-                    <input type="text" id="output_curr" placeholder=" output"/>
-                    <select id="pick_output">
+                    <input type="text" id="output_curr" placeholder={this.state.result}/>
+                    <select id="pick_output" onChange={this.handleChangeOutputSelect}>
                     <option value="USD">USD</option>
                     <option value="EURO">EURO</option>
                     <option value="UAH">UAH</option>
